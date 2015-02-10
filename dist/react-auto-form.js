544edf0e1daef2dcddfa132b270771a219330ef5
Jonny Buchanan <jonathan.buchanan@gmail.com>
2015-02-10 10:35:39 +0000

    v1.0.0

--- dist/react-auto-form.js
+++ dist/react-auto-form.js
@@ -1,5 +1,5 @@
 /*!
- * react-auto-form 1.0.0 (dev build at Mon, 09 Feb 2015 21:08:26 GMT) - https://github.com/insin/react-auto-form
+ * react-auto-form 1.0.0 - https://github.com/insin/react-auto-form
  * MIT Licensed
  */
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var o;"undefined"!=typeof window?o=window:"undefined"!=typeof global?o=global:"undefined"!=typeof self&&(o=self),o.AutoForm=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
